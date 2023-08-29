413625f982f98211f3683b7bec6836a30e72b2d5
marouane60 <marouanehilal@Marouanes-Laptop.local>
2023-08-29 12:51:18 +0200

    pref-paris

--- main.py
+++ main.py
@@ -3,31 +3,40 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-
-
-
-def check_for_appointment():
-    pref_link = "https://www.hauts-de-seine.gouv.fr/booking/create/8485/0"
-
-    ## create an object of the chrome webdriver
-    driver = webdriver.Chrome(executable_path=r'./chromedriver')
-    ## open selenium URL in chrome browser
-    driver.get(pref_link)
-
-    driver.find_element(By.ID, "condition").click()
+from webdriver_manager.chrome import ChromeDriverManager
+#"planning1075"
+def select_guichet(guichet_id, driver):
+    driver.find_element(By.ID, guichet_id).click()
     time.sleep(5)
     driver.find_element(By.NAME, "nextButton").click()
     time.sleep(5)
+def check_for_appointment(guichet):
+    pref_link = "https://pprdv.interieur.gouv.fr/booking/create/989/1"
 
-    if(driver.page_source.find("plus de plage") < 0):
-        os.system("say rendez vous rab bek")
+    ## create an object of the chrome webdriver
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    ## open selenium URL in chrome browser
+    driver.get(pref_link)
+
+    select_guichet(guichet, driver)
+
+    if (driver.page_source.find("plus de plage") < 0):
+        return True
     else:
-        check_for_appointment()
+        driver.quit()
+        return False
 
-
+def check_all_guichets():
+    for guichet in ["planning1075", "planning1076", "planning1077"]:
+        if(check_for_appointment(guichet)):
+            return True
+    return False
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    check_for_appointment()
+    while(check_all_guichets() == False):
+        check_all_guichets()
+
+    os.system("say rendez vous rab bek")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
